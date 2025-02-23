78d0baf8ce77ca8830a7d0fe85e67f944e555b23
korpsthecoder31 <korpsthewriter@gmail.com>
2025-02-23 18:58:18 +0800

    Exit play_game when game_over
    	- add @winner instance and set to false
    	- if @black_pegs == 4, @winner is true, break while loop
    	- create game_over method
    	- if winner is true, game_over puts CONGRATS else YOU LOSE

--- lib/game.rb
+++ lib/game.rb
@@ -6,11 +6,16 @@ require_relative 'computer'
 
 module Mastermind
   class Game
+    attr_accessor :black_pegs, :white_pegs, :winner
+
     def initialize
       @choices = []
       @guesses = []
       @guesser = Human.new
       @chooser = Computer.new
+      @black_pegs = 0
+      @white_pegs = 0
+      @winner = false
       play_game
     end
 
@@ -21,18 +26,20 @@ module Mastermind
         @guesses = @guesser.guess_colors
         compare(@choices, @guesses)
         attempts += 1
+        break if @winner == true
       end
+      game_over
     end
 
     def compare(choices, guesses)
         choices_copy = choices.dup
         guesses_copy = guesses.dup
-        black_pegs = 0
-        white_pegs = 0
+        @black_pegs = 0
+        @white_pegs = 0
   
       guesses_copy.each_with_index { |color, index|
         if (color == choices_copy[index])
-          black_pegs += 1
+          @black_pegs += 1
           choices_copy[index] = nil
           guesses_copy[index] = nil
         end
@@ -40,12 +47,24 @@ module Mastermind
 
       guesses_copy.compact.each { |color|
         if choices_copy.include?(color)
-          white_pegs += 1
+          @white_pegs += 1
           index = choices_copy.index(color)
           choices_copy[index] = nil
         end
       }
-      puts "Black Pegs: #{black_pegs} White Pegs: #{white_pegs}"
+      puts "Black Pegs: #{@black_pegs} White Pegs: #{@white_pegs}"
+
+      if @black_pegs == 4 
+        @winner = true
+      end
+    end
+
+    def game_over
+      if @winner == true
+        puts "CONGRATS"
+      else
+        puts "YOU LOSE"
+      end
     end
   end
 end
